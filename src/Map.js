1c4a5f512eaa22fe7abc4362dccb433d5a8ddb65
AliceR <alice.ruehl@gmail.com>
2018-09-01 17:43:32 +0200

    Add Map

---
+++ src/Map.js
@@ -0,0 +1,33 @@
+import mapboxgl from 'mapbox-gl'
+import React from 'react'
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoidXN0cm9ldHoiLCJhIjoiQmp3RjlaZyJ9.7JCU4lzvAzfijEV129QFiQ'
+
+class Map extends React.Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/outdoors-v9',
+      center: [10.1299, 51.3401],
+      zoom: 5.1
+    })
+  }
+
+  componentWillUnmount() {
+    this.map.remove()
+  }
+
+  render() {
+    const style = {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      width: '100%'
+    }
+
+    return <div style={style} ref={el => (this.mapContainer = el)} />
+  }
+}
+
+export default Map
