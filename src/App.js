6ff273162f797e1826d17ba41ecedfe2bb58fe08
ustroetz <ustroetz@gmail.com>
2018-09-02 18:32:43 +0200

    Add range slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Drawer from '@material-ui/core/Drawer'
 import { withStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { Range } from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import React, { Component } from 'react'
 
 import Map from './Map'
@@ -28,10 +30,30 @@ const styles = theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     minWidth: 0 // So the Typography noWrap works
+  },
+  range: {
+    margin: 20,
+    top: 100,
+    position: 'relative'
   }
 })
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      min: 50000,
+      max: 250000
+    }
+  }
+
+  onSliderChange = value => {
+    this.setState({
+      min: value[0],
+      max: value[1]
+    })
+  }
+
   render() {
     const { classes } = this.props
     return (
@@ -43,7 +65,20 @@ class App extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }} />
+        <Drawer variant="permanent" classes={{ paper: classes.drawerPaper }}>
+          <div className={classes.range}>
+            <Typography variant="subheading" color="inherit" align="center">
+              👩‍🌾 {this.state.min} - {this.state.max} 👩‍⚕️👩‍💻👨‍🎨🤵👩‍🚀
+            </Typography>
+            <Range
+              defaultValue={[this.state.min, this.state.max]}
+              min={0}
+              max={500000}
+              step={1000}
+              onChange={this.onSliderChange}
+            />
+          </div>
+        </Drawer>
         <main className={classes.content}>
           <Map />
         </main>
