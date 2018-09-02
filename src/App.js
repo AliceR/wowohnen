7faaf4d3bdee758d9a6a0253b9f2effbcfd85761
AliceR <alice.ruehl@gmail.com>
2018-09-02 14:30:02 +0200

    Sort imports alphabetically based on lib

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import AppBar from '@material-ui/core/AppBar'
 import Drawer from '@material-ui/core/Drawer'
-import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import React, { Component } from 'react'
 
 import Map from './Map'
 
